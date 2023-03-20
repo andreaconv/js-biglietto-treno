4aaecdf653fe554b5525536a886f7d2891a16f41
andreaconv <andrea2convertini@gmail.com>
2023-03-20 17:27:51 +0100

    [add] calcolo biglietto

---
+++ JS/script.js
@@ -0,0 +1,28 @@
+const km = parseInt(prompt("Quanti chilometri vuoi percorrere?", 86));
+console.log(km);
+
+const anni = parseInt(prompt("Quani anni hai?", 67));
+console.log(anni);
+
+//PREZZO COMPLETO
+let prezzocompleto = km * 0.21;
+console.warn(prezzocompleto);
+
+//CALCOLO EVENTUALE SCONTO IN BASE ALL'ETÀ
+if (anni < 18) {
+  prezzocompleto = prezzocompleto - (prezzocompleto * 0.2);
+}else if (anni > 65) {
+  prezzocompleto = prezzocompleto - (prezzocompleto * 0.4);
+}
+
+console.log(prezzocompleto);
+
+
+// LET da collegare a HTML
+let outputStr = `
+  Il prezzo del tuo biglietto è ${prezzocompleto}
+`
+
+
+// COLLEGAMENTO a HTML 
+document.getElementById("output").innerHTML = outputStr;
